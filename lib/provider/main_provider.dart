08b39462a671d67c9be2b133ec865b03350d1c7e
IzumiShaka-desu <shakaaji29@gmail.com>
2022-01-06 17:40:10 +0700

    add loadNotes method

--- lib/provider/main_provider.dart
+++ lib/provider/main_provider.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:secret_box/datasource/hive_datasource.dart';
+import 'package:secret_box/model/note.dart';
 
 class MainProvider extends ChangeNotifier {
   HiveDataSource hiveDS = HiveDataSource();
   bool _isLogin = false;
   bool get isLogin => _isLogin;
+  List<Note> _notes = [];
+  List<Note> get notes => _notes;
   Future<bool> openBox(String boxName, String password) async {
     _isLogin = await hiveDS.openBox(boxName, password);
+    notifyListeners();
     return _isLogin;
+  }
+
+  void loadNotes() async {
+    _notes = await hiveDS.getNotes();
+    notifyListeners();
   }
 }
