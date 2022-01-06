8f6115081d11c76793bf3a341e1e21abe3b33cb4
IzumiShaka-desu <shakaaji29@gmail.com>
2022-01-06 17:36:11 +0700

    add openBox method on mainProvider

--- lib/provider/main_provider.dart
+++ lib/provider/main_provider.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:secret_box/datasource/hive_datasource.dart';
 
 class MainProvider extends ChangeNotifier {
+  HiveDataSource hiveDS = HiveDataSource();
   bool _isLogin = false;
   bool get isLogin => _isLogin;
+  Future<bool> openBox(String boxName, String password) async {
+    _isLogin = await hiveDS.openBox(boxName, password);
+    return _isLogin;
+  }
 }
