01c98b4fb8069c1ad0e0db9b7c14be1e093e34c6
IzumiShaka-desu <shakaaji29@gmail.com>
2022-01-06 17:23:44 +0700

    styling login view

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -21,20 +21,54 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Row(children: const [
+          Icon(Icons.visibility_off),
+          Text("Secret Box"),
+        ]),
+      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            TextFormField(
-              controller: boxNameController,
-            ),
-            TextFormField(
-              controller: passwordController,
-            ),
-            MaterialButton(
-              onPressed: () {},
-              child: const Text("Open Box"),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(top: 18.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const Text(
+                "Open your Box",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      labelText: "Box Name",
+                      hintText: "Input your box name.. ex: box-patrick"),
+                  controller: boxNameController,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      labelText: "Password",
+                      hintText: "Input your password.."),
+                  controller: passwordController,
+                ),
+              ),
+              MaterialButton(
+                color: Theme.of(context).primaryColor,
+                onPressed: () {},
+                child: const Text("Open Box"),
+              )
+            ],
+          ),
         ),
       ),
     );
