37a348178a86c507cf7e3120e79c5ea4d1ec2a82
IzumiShaka-desu <shakaaji29@gmail.com>
2022-01-06 13:23:36 +0700

    initialize form

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
   static const String routeName = "/login";
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  late final TextEditingController boxNameController;
+  late final TextEditingController passwordController;
+  @override
+  void initState() {
+    boxNameController = TextEditingController();
+    passwordController = TextEditingController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            TextFormField(
+              controller: boxNameController,
+            ),
+            TextFormField(
+              controller: passwordController,
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: const Text("Open Box"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
