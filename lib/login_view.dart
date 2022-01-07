6889ff2020238f8f967bd559f8781d514c4cdcf0
IzumiShaka-desu <shakaaji29@gmail.com>
2022-01-07 23:31:48 +0700

    add navigation

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
+import 'package:secret_box/home_view.dart';
+import 'package:secret_box/provider/main_provider.dart';
+
+import 'test_view.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -11,6 +16,7 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   late final TextEditingController boxNameController;
   late final TextEditingController passwordController;
+
   @override
   void initState() {
     boxNameController = TextEditingController();
@@ -19,14 +25,27 @@ class _LoginViewState extends State<LoginView> {
   }
 
   @override
+  void dispose() {
+    boxNameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final prov = context.read<MainProvider>();
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Row(children: const [
-          Icon(Icons.visibility_off),
-          Text("Secret Box"),
-        ]),
+        title: InkWell(
+          onTap: () {
+            Navigator.of(context).pushNamed(TestView.routeName);
+          },
+          child: Row(children: const [
+            Icon(Icons.visibility_off),
+            Text("Secret Box"),
+          ]),
+        ),
       ),
       body: SafeArea(
         child: Padding(
@@ -64,7 +83,33 @@ class _LoginViewState extends State<LoginView> {
               ),
               MaterialButton(
                 color: Theme.of(context).primaryColor,
-                onPressed: () {},
+                onPressed: () async {
+                  if (boxNameController.text.isEmpty &&
+                      passwordController.text.length < 8) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text(
+                            "please fill all fields and password length greater than 7"),
+                      ),
+                    );
+                  } else {
+                    bool isCanOpen = await prov.openBox(
+                      boxNameController.text,
+                      passwordController.text,
+                    );
+                    if (isCanOpen) {
+                      prov.loadNotes();
+                      Navigator.pushReplacementNamed(
+                          context, HomeView.routeName);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("something error please try again"),
+                        ),
+                      );
+                    }
+                  }
+                },
                 child: const Text("Open Box"),
               )
             ],
